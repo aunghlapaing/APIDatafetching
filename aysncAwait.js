daca1e1b2e0c5cd9d5f5c832b8c5a6b4fe3aa731
aunghlapaing <aunghlapaing8949@gmai.com>
2024-11-21 10:34:35 +0000

    API Fetching with aysncAwait

---
+++ aysncAwait.js
@@ -0,0 +1,36 @@
+let table = document.getElementById ('tableData');
+
+let dataFetching = async () => {
+    var temp = "";
+    let dataFetch = await fetch ("https://fakestoreapi.com/products") //fetch data from API link
+    products = await dataFetch.json();
+    products.forEach(element => {
+        temp += `
+        <tbody>
+            <tr>
+                <td>${element.id}</td>
+                <td>${element.title}</td>
+                <td>${element.description}</td>
+                <td><img style="width:20px; heigh:20px" src="${element.image}"></img></td>
+                <td>${element.price}</td>
+            </tr>
+        </tbody>
+        `
+        table.innerHTML = `
+
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Image</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        `+temp;
+        console.log(products);
+        
+        
+    });
+    // dataFetching();
+}
